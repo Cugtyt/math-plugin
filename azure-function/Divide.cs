99a446fc6dd62e0a3a2dbf872286149a038ef3b7
Cugtyt <cugtyt@qq.com>
2023-08-29 16:26:41 +0800

    init

---
+++ azure-function/Divide.cs
@@ -0,0 +1,53 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Globalization;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.OpenApi.Models;
+
+namespace MathPlugin
+{
+    public class Divide
+    {
+        private readonly ILogger _logger;
+
+        public Divide(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Add>();
+        }
+
+        [Function("Divide")]
+        [OpenApiOperation(operationId: "Divide", tags: new[] { "ExecuteFunction" }, Description = "Divides two numbers.")]
+        [OpenApiParameter(name: "number1", Description = "The first number", Required = true, In = ParameterLocation.Query)]
+        [OpenApiParameter(name: "number2", Description = "The second number", Required = true, In = ParameterLocation.Query)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "Returns the divided result of the two numbers.")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "application/json", bodyType: typeof(string), Description = "Returns the error of the input.")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            bool result1 = double.TryParse(req.Query["number1"], out double number1);
+            bool result2 = double.TryParse(req.Query["number2"], out double number2);
+
+            if (result1 && result2)
+            {
+                HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+                response.Headers.Add("Content-Type", "text/plain");
+                double result = number1 / number2;
+                response.WriteString(result.ToString(CultureInfo.CurrentCulture));
+
+                _logger.LogInformation($"Numbers are {number1} and {number2}");
+                _logger.LogInformation($"Add function processed a request. Result: {result}");
+
+                return response;
+            }
+            else
+            {
+                HttpResponseData response = req.CreateResponse(HttpStatusCode.BadRequest);
+                response.Headers.Add("Content-Type", "application/json");
+                response.WriteString("Please pass two numbers on the query string or in the request body");
+
+                return response;
+            }
+        }
+    }
+}
---
+++ plugin-test/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Planning;
+using Microsoft.SemanticKernel.Skills.OpenAPI.Extensions;
+
+// ... create a new Semantic Kernel instance here
+IKernel kernel = Kernel.Builder.WithAzureChatCompletionService(
+    "openai-deployment",
+    "https://xxx.openai.azure.com/",
+    "the key"
+    ).Build();
+
+// Add the math plugin using the plugin manifest URL
+const string pluginManifestUrl = "http://localhost:7071/.well-known/ai-plugin.json";
+var mathPlugin = await kernel.ImportAIPluginAsync("MathPlugin", new Uri(pluginManifestUrl));
+
+// Create a stepwise planner and invoke it
+var planner = new StepwisePlanner(kernel);
+var question = "I have $2130.23. How much would I have after it grew by 24% and after I spent $5 on a latte?";
+var plan = planner.CreatePlan(question);
+var result = await plan.InvokeAsync(kernel.CreateNewContext());
+
+// Print the results
+Console.WriteLine("Result: " + result);
+
+// Print details about the plan
+if (result.Variables.TryGetValue("stepCount", out string? stepCount))
+{
+    Console.WriteLine("Steps Taken: " + stepCount);
+}
+if (result.Variables.TryGetValue("skillCount", out string? skillCount))
+{
+    Console.WriteLine("Skills Used: " + skillCount);
+}
